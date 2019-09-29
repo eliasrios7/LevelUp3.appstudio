ac592ec71088f1555d31ea02e511c817873069c5
eliasrios7 <55107200+eliasrios7@users.noreply.github.com>
2019-09-29 18:29:33 -0500

    Update Form1.js
    Completed the car loan function

--- forms/Form1/Form1.js
+++ forms/Form1/Form1.js
@@ -1,4 +1,32 @@
-Hello
-Can you see this?
+/*
+//a,b,c are local variables inside the function
+function findAverage(a, b, c) {
+    let average = (a + b + c)/3
+    return average
+}
 
-Is this change in the carloan branch?
+// main program
+let myAnswer = findAverage(10,20,30)
+alert(myAnswer)
+*/
+//Car Loan Payment Branch info
+
+//Function Program
+function monthlyPayment(p, i, n) {
+  return p * i * (Math.pow(1+i,n)) / (Math.pow(1+i,n)-1)
+  }
+
+//Main Program
+let principle = 200000
+let interest = .057 / 12
+let numPay = 30 * 12
+
+let myAnswer = monthlyPayment(principle, interest, numPay)
+console.log(myAnswer)
+
+
+  
+  
+  
+  
+
